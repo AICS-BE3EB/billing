58106c83032640814ffd86d96442159f38761057
rickyx12 <aizekrysler@gmail.com>
2017-07-21 00:11:25 +0800

    UI for navbar

---
+++ views/navbar/navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+  
+    <div class="col-md-6">
+	     <form class="navbar-form">
+	        <div class="form-group" style="display:inline;">
+	          <div class="input-group" style="display:table;">
+	            <span class="input-group-addon" style="width:1%;"><span class="glyphicon glyphicon-search"></span></span>
+	            <input class="form-control" id="patientSearch" name="search" placeholder="Search Patient" autocomplete="off" autofocus="autofocus" type="text">
+	          </div>
+	        </div>
+	      </form>	    
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+    	<li>
+    		<a href="../session/logout.php">
+    			<i class="glyphicon glyphicon-user"></i>
+    			Logout
+    		</a>
+    	</li>
+    </ul>
+  </div>
+</nav>
