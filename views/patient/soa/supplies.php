b414b3df047b053930b482d7bb700ed3e4d610f2
rickyx12 <aizekrysler@gmail.com>
2017-07-30 13:50:04 +0800

    fix number format and date format

--- views/patient/soa/supplies.php
+++ views/patient/soa/supplies.php
@@ -1,13 +1,22 @@
 <?php
 	$patient->getChargesByTitle($registrationNo,'SUPPLIES');
+	$suppliesTotal = 0;
 ?>
 
 <?php foreach( $patient->getChargesByTitleItemNo() as $itemNo ): ?>
 	<tr>
-		<td><?= $db->selectNow('patientCharges','dateCharge','itemNo',$itemNo) ?></td>
+		<?php $suppliesTotal += $db->selectNow('patientCharges','total','itemNo',$itemNo) ?>
+		<td><?= $db->formatDate($db->selectNow('patientCharges','dateCharge','itemNo',$itemNo)) ?></td>
 		<td><?= $db->selectNow('patientCharges','description','itemNo',$itemNo) ?></td>
 		<td><?= $db->selectNow('patientCharges','quantity','itemNo',$itemNo) ?></td>
-		<td><?= $db->selectNow('patientCharges','sellingPrice','itemNo',$itemNo) ?></td>
-		<td><?= $db->selectNow('patientCharges','total','itemNo',$itemNo) ?></td>
+		<td><?= $db->number_format($db->selectNow('patientCharges','sellingPrice','itemNo',$itemNo)) ?></td>
+		<td><?= $db->number_format($db->selectNow('patientCharges','total','itemNo',$itemNo)) ?></td>
 	</tr>
 <?php endforeach; ?>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td><b><?= $db->number_format($suppliesTotal) ?></b></td>
+	</tr>
